7cd0a1dd5c4453520cc55a4bdc0185263718b585
unknown <akobets@TTWIN7AK01.corp.talenttech.com>
2012-08-14 07:46:36 -0400

    add console application

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            puts("Hello world");
+        }
+        static void puts(string text)
+        {
+            System.Console.Out.WriteLine(text);
+        }
+    }
+
+}
